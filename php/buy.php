287967b624ff0941e4501a5645f08cf6b1387fe8
lucashsu95 <lucashsu95@gmail.com>
2022-12-30 21:40:09 +0800

    2022/12/30

--- php/buy.php
+++ php/buy.php
@@ -1,5 +1,17 @@
 <?php
-// $db = new PDO('mysql:host=localhost;dbname=web01','root','','');
-echo $_POST['Lsum'];
-// $sql = prepare('insert into buy (product,sum,name,address,content) values(:pd,:sum,:name,:address,:content');
-// $sql->bindValue('pd',$_POST['pd']);
+$db = new PDO('mysql:host=localhost;dbname=carbon_waste','admin','1234');
+$sql = $db->prepare('insert into buy(Lproduct_name,Lsum,Lname,Laddress,Lcontent) values(:pd,:sum,:name,:address,:content)');
+
+$product = '';
+foreach($_POST as $name => $amount){
+    if($name == 'name')break;
+    $product .= $name . $amount . '@';
+}
+
+$sql->bindValue('pd',$product);
+$sql->bindValue('sum',$_POST['Lsum']);
+$sql->bindValue('name',$_POST['name']);
+$sql->bindValue('address',$_POST['address']);
+$sql->bindValue('content',$_POST['content']);
+$sql->execute();
+header('location:.././');
--- php/information.php
+++ php/information.php
@@ -1,3 +1,8 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=web01','root','','');
-
+$db = new PDO('mysql:host=localhost;dbname=carbon_waste','admin','1234');
+$sql=$db->prepare('insert into information(username,usergmail,usercontent) values(:un,:ug,:uc)');
+$sql->bindValue('un',$_POST['username']);
+$sql->bindValue('ug',$_POST['usergmail']);
+$sql->bindValue('uc',$_POST['usercontent']);
+$sql->execute();
+header('location:.././');
