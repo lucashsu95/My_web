e5829c2b369b61fc7c5d21dd8904218653dbf44b
lucashsu95 <lucashsu95@gmail.com>
2023-02-06 00:31:32 +0800

    2023/2/6

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ let oldTop = 0;
 // console.log(nav.classList);
 window.onscroll = () => {
     let nowTop = document.documentElement.scrollTop;
-    if(nowTop > oldTop){
+    if (nowTop > oldTop) {
         nav.style.transform = 'translateY(-100%);';
-    }else{
+    } else {
         nav.style.transform = 'translateY(0);';
     }
     oldTop = nowTop;
@@ -102,7 +102,7 @@ const app = Vue.createApp({
                 }
             ],
             shopingCart_toggle_img: "<i class='bi bi-cart4'></i>",
-            select_container:['全部','減脂系列','增肌系列','豪華系列'],
+            select_container: ['全部', '減脂系列', '增肌系列', '豪華系列'],
             products: [{
                 // 減脂系列
                 img: './images/元氣滿滿雞肉餐.jpg',
@@ -184,7 +184,6 @@ const app = Vue.createApp({
                 carbon_content: "牛肉 60kg/1 kg"
             }, {
                 // 豪華系列
-
                 img: './images/元氣滿滿烤鮭魚.jpg',
                 name: '豪華滿滿烤鮭魚',
                 price: 180,
@@ -202,27 +201,37 @@ const app = Vue.createApp({
             ],
             slide_id: 0,
             slide: [{
-                title: '年齡層的影響',
-                english: 'Age group',
-                content: '年齡層一般女生會多於男生，年齡層的話大概都是普遍上班族，像以年輕人的角度來講吃的口味一般就會比較重，所以比較容易乎略飲食的均衡問題，低碳餐飲客的喜愛度會很高，固定的客源，回頭客回頭率很高。',
-                btn: '搶先體驗',
-                color: 'text-danger',
+                title: '碳中和',
+                english: 'Carbon Neutralp',
+                content: `「碳中和」（Carbon Neutral）指的是，企業、組織或政府在特定一段時間的二氧化碳排放量，透過植樹、使用再生能源等方式累積的減碳量相互抵銷、清除。
+                不同於淨零排放是強調「人為造成的溫室氣體排放量」。 也就是說，在特定期間內，「碳排放量」與「碳清除量」相等、無增無減，達成「碳中和」的狀態。
+                `,
+                color: '#7da9d5d9',
                 class: 'btn-outline-danger'
             },
             {
-                title: '價格方面',
-                english: 'Price',
-                content: '明明健康餐的東西既不調味又不油炸，為什麼這麼貴，像是低碳飲食類的東西，其實這些飲食他會變貴是有一個有個很大的原因就是食材的新鮮度、而且沒有調味食材就要更新鮮，才會讓人覺得好吃。',
-                btn: '會員登入',
-                color: 'text-success',
+                title: '淨零排放',
+                english: 'Net Zero',
+                content: `「淨零排放」指的是在特定一段時間內，全球人為造成的溫室氣體排放量，扣除人為移除的量等於零。 也就是，淨零並不是不排放，而是盡力讓人為造成的溫室氣體排放極小化。
+                為了達到淨零排放，許多政府和企業會透過負碳技術（利用碳捕集封存、再利用CCS、CCUS）、自然碳匯（森林碳匯、海洋吸附）等方式抵消碳排放。
+                `,
+                color: '#c3c076',
                 class: 'btn-outline-success'
             },
             {
-                title: '食物的出產地',
-                english: 'Origin',
-                content: '我們都採用在地的新鮮食材，我們家的豬肉吃起來是沒有腥味的那種，我們的活菌豬吃的飼料跟一般的不一樣，這也就是我們低碳便當會比一般便當貴的原因，想要健康又想要好吃，食材就要新鮮，選擇在地的食材。',
-                btn: '聯絡我們',
-                color: 'text-info',
+                title: '台灣的淨零碳排政策',
+                english: 'Taiwan',
+                content: '2022年3月30日，在眾人的期盼下，行政院國家發展委員會公布了「台灣2050淨零排放路徑及策略」，預計在2030年前編列約9000億預算，執行能源、產業、生活、社會四大轉型策略，以及科技研發、氣候法制兩大治理基礎，並輔以12項關鍵戰略，計畫於2050年與世界同步邁向淨零目標。',
+                color: '#d3a5a5',
+                class: 'btn-outline-info'
+            },
+            {
+                title: '低碳飲食',
+                english: 'Low Carb Diet',
+                content: `低碳飲食是在每日飲食中減少碳水化合物的攝取量，並提高蛋白質、油脂、纖維質在飲食中的比重，低碳飲食全名為低碳水化合物飲食，也稱作低醣飲食，顧名思義就是降低碳水化合物，也就是醣類的攝取量，提高天然蛋白質及優良脂質的攝取。
+                低碳飲食近年來很熱門，它沒有生酮飲食嚴格、也不需要精確計算熱量，是比較彈性、簡單的減重方式。
+                `,
+                color: '#b1aacb',
                 class: 'btn-outline-info'
             }],
             computer_now: 'first',
@@ -373,6 +382,22 @@ const app = Vue.createApp({
                 class: 'sixth',
                 bg: '#c99f63',
             }],
+            information: [{
+                title: '年齡層的影響',
+                english: 'Age group',
+                content: '年齡層一般女生會多於男生，年齡層的話大概都是普遍上班族，像以年輕人的角度來講吃的口味一般就會比較重，所以比較容易乎略飲食的均衡問題，低碳餐飲客的喜愛度會很高，固定的客源，回頭客回頭率很高。',
+            },
+            {
+                title: '價格方面',
+                english: 'Price',
+                content: '明明健康餐的東西既不調味又不油炸，為什麼這麼貴，像是低碳飲食類的東西，其實這些飲食他會變貴是有一個有個很大的原因就是食材的新鮮度、而且沒有調味食材就要更新鮮，才會讓人覺得好吃。',
+            },
+            {
+                title: '食物的出產地',
+                english: 'Origin',
+                content: '我們都採用在地的新鮮食材，我們家的豬肉吃起來是沒有腥味的那種，我們的活菌豬吃的飼料跟一般的不一樣，這也就是我們低碳便當會比一般便當貴的原因，想要健康又想要好吃，食材就要新鮮，選擇在地的食材。',
+            }],
+
             test_change: 0,
             sorts: ['A', 'B', 'C', 'D'],
             ans: ['@', 'C', 'A', 'B', 'D', 'A'],
@@ -425,7 +450,7 @@ const app = Vue.createApp({
             this.computers.forEach(computer => {
                 compute_total += computer.amount * computer.carbon
             });
-            compute_total = compute_total != 0 ? `總排碳量：${compute_total.toFixed(1)}g` : '可點擊上方圖片變換食物種類';
+            compute_total = compute_total != 0 ? `總排碳量：${compute_total.toFixed(1)}g` : '';
             return compute_total
         },
         test_result() {
