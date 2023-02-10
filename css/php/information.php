8d55a8d3b6976791dc45f862b6faaf71a5bc6fa2
lucashsu95 <lucashsu95@gmail.com>
2023-02-10 20:48:14 +0800

    2023/2/10

--- css/php/information.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=carbon_waste','admin','1234');
-$sql=$db->prepare('insert into information(username,usergmail,usercontent) values(:un,:ug,:uc)');
-$sql->bindValue('un',$_POST['username']);
-$sql->bindValue('ug',$_POST['usergmail']);
-$sql->bindValue('uc',$_POST['usercontent']);
-$sql->execute();
-header('location:.././');
